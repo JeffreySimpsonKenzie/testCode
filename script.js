ed7af3b18e369247e68599c631e10c6a47d77970
JeffreySimpsonKenzie <jeffrey.simpson@kenzie.academy>
2021-06-07 13:38:24 -0400

    Add some addition tags for a demo

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Now this is podracing!");
+console.log("I'm just looking for love in all the wrong places....");
+let aString = "everything";
+
+aString = "now this";
+
+aString = "everything new";
+
+aString = "a new String";
