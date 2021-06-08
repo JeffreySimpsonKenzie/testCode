3377392515c73d2a9a47755d1cec2d0e54eb80f2
JeffreySimpsonKenzie <jeffrey.simpson@kenzie.academy>
2021-06-08 09:56:56 -0400

    Added various info on VS Code Shortcuts

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-console.log("I'm just looking for love in all the wrong places....");
-let aString = "everything";
+console.log("Well look at that, this is showing up in your console!");
 
-aString = "now this";
+/* 
+Add quotes to a selection of Code.
+1. Select/Highlight a section of code.
+2. Press the ' or " key.
 
-aString = "everything new";
+TRY IT OUT! Change the number below to a string.
+The the console should show "testNumber variable is a string".
+*/
+let testNumber = 123456789;
+console.log(testNumber);
+console.log("testNumber variable is a", typeof testNumber);
 
-aString = "a new String";
+/*
+Add (), {}, [] to a selection of code.
+1. Select/Highlight a section of code.
+2. Press (, {, [ key.
+
+TRY IT OUT!
+Add various brackets to the string below using this new shortcut!
+
+It should look similar to this:
+"Add () around this: (parentheses)  Add {} around this: {curly brackets}, or add [] around this: [square brackets] "
+*/
+
+let testString =
+  "Add () around this: parentheses  Add {} around this: curly brackets, or add [] around this: square brackets ";
+
+console.log(testString);
+
+/*
+You can select multiple instances of the same code with a simple keyboard shortcut.
+1. Select/Highlight a section of code.
+2.Press the combo keys, for Mac: cmd + D  and for Windows: ctrl + D
+
+TRY IT OUT! Below is a variable used multiple times in the code.
+Use the new selector shortcut to change the variable's name on all its instances.
+*/
+
+let thisVariable = "I am a string!";
+
+console.log(thisVariable);
+
+thisVariable = "Now I'm a different string!";
+
+console.log(thisVariable);
